4a5ac4e1387e8fcec175a782f5d6209dedfbe2e8
sid5233 <sudarshan.bhamare@gmail.com>
2022-09-07 11:28:34 +0530

    Day 1 completed.

---
+++ Day1/main.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+#input("Hey who are you? ")
+
+#you will take input first and then print will execute.
+
+# print("Hey" + input("Who are you"))
+
+
+print(len(input("write your name and see magic ")))
---
+++ Day1/name_generator.py
@@ -0,0 +1,3 @@
+name = input("what is you name? \n")
+city = input("what is you city? \n")
+print("Hey " + name + " " + "from " +  city)
