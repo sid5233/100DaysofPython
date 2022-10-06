0214540d45b49615042daa7e2dafd35c9a5095f3
sid5233 <sudarshan.bhamare@gmail.com>
2022-10-07 00:24:05 +0530

    fetched data

--- Day4_2/automation.py
+++ Day4_2/automation.py
@@ -1,2 +1,14 @@
 import openpyxl
 
+myfile = openpyxl.load_workbook("demo.xlsx")
+print(myfile)
+data = myfile["Sheet1"]
+print(data)
+
+noOfProductDict = {}
+
+for i in range(2,data.max_row + 1):
+    nameSupplier = data.cell(i,4)
+    
+    noOfProductDict["nameSupplier"] = 1
+    
