1183c984cb8cd1eb23ea0fc03c3eb7c3e75403cd
sid5233 <sudarshan.bhamare@gmail.com>
2022-10-09 23:22:49 +0530

    created VPC

--- Day1_4/main.py
+++ Day1_4/main.py
@@ -3,6 +3,28 @@ from pydoc import cli
 import boto3
 
 client = boto3.client('ec2', region_name="eu-central-1")
+resour = boto3.resource('ec2', region_name="eu-central-1")
+
+newVPC = resour.create_vpc(
+    CidrBlock="10.0.0.0/16"
+)
+
+newVPC.create_subnet(
+    CidrBlock = "10.0.1.0/24"
+)
+
+newVPC.create_subnet(
+    CidrBlock = "10.0.2.0/24"
+)
+
+newVPC.create_tags(
+    Tags=[
+        {   
+        'Key': 'Name',
+        'Value' : 'my-vpc'
+        }
+    ]
+)
 
 allVPC = client.describe_vpcs()
 vpcs = allVPC["Vpcs"]
