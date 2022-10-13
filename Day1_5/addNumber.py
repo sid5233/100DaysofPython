972c01a1886604e3e8e750c8c1bb6a8d1b5fa8a8
sid5233 <sudarshan.bhamare@gmail.com>
2022-10-14 00:59:41 +0530

    basic of operators

---
+++ Day1_5/addNumber.py
@@ -0,0 +1,14 @@
+num_char = input("write a num\n")
+
+# strNum = str(num_char)
+
+
+#print(strNum)
+num1 = num_char[0]
+
+num2 = num_char[1]
+
+total = int(num1) + int(num2)
+print(total)
+
+#print("your name has " + new_num + " char")
